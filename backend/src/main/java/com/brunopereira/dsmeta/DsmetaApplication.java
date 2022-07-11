c11861391c2369ca6d37173e5e5c82ac768536c2
Bruno Pereira <brunopereiradosreis369@gmail.com>
2022-07-11 12:26:16 -0300

    Project created

---
+++ backend/src/main/java/com/brunopereira/dsmeta/DsmetaApplication.java
@@ -0,0 +1,13 @@
+package com.brunopereira.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaApplication.class, args);
+	}
+
+}
