e06e88d566be389f39210c189fdbb5a37c1d6a43
Bruno Pereira <brunopereiradosreis369@gmail.com>
2022-07-11 15:48:27 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+        <div className="dsmeta-logo-container">
+            <img src={logo} alt="DSMeta"/>
+            <h1>DSmeta</h1>
+            <p>Desenvolvido por <a href="">@BrunoPereira</a>
+            </p>
+        </div>
+    </header>
+    )
+   }
+   
+   export default Header
